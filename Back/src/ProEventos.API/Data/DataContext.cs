52a04808a523e761ee824099732202d2f8bf5745
kastjr <108149154+kastjr@users.noreply.github.com>
2023-01-13 18:37:48 -0300

    adição do bd

--- Back/src/ProEventos.API/Data/DataContext.cs
+++ Back/src/ProEventos.API/Data/DataContext.cs
@@ -5,8 +5,8 @@ namespace Back.src.ProEventos.API.Data
 {
     public class DataContext : DbContext 
     {
-        public DataContext(DbContextOptions<DataContext> options) : base() {}
-        
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+
         public DbSet<Evento> Eventos { get; set; }
     }
 }
