7eb87c95c4b9f0751278b7c4448294cd80d34bde
kastjr <108149154+kastjr@users.noreply.github.com>
2023-03-25 21:51:07 -0300

    final da seção 5

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos : any ;
+  public eventosFiltrados: any = [];
+  public eventos : any = [];
+  larguraimg: number=150;
+  margemimg: number=2;
+  mostrarimg: boolean=true;
+  private _filtrolista: string = '';
+
+  public get filtrolista(): string {
+    return this._filtrolista;
+  }
+
+  public set filtrolista(value: string) {
+    this._filtrolista=value;
+    this.eventosFiltrados = this.filtrolista ? this.filtrarEventos(this.filtrolista) : this.eventos
+  }
+
+  filtrarEventos (filtrarPor: string) : any{
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -16,10 +38,16 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  alterarImg(){
+    this.mostrarimg = !this.mostrarimg
+  }
+
   public getEventos(): void {
-    this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
-      error => console.log(error),
+    this.http.get("https://localhost:5001/api/Eventos").subscribe(
+      response => {this.eventos = response;
+                   this.eventosFiltrados = this.eventos
+      },
+      error => console.log(error)
     );
 
 
